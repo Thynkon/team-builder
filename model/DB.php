2f3c2aaabcd9d56e731c4768b9820402ebc0b5ff
thynkon <thynkon@protonmail.com>
2021-09-14 14:43:11 +0200

    Add DB ORM and tests

---
+++ model/DB.php
@@ -0,0 +1,77 @@
+<?php
+
+require_once('.env.php');
+
+class DB
+{
+
+    static private function getDbConnection()
+    {
+        return new PDO(DSN, USERNAME, PASSWORD);
+    }
+
+    static public function selectMany($query, $args)
+    {
+        $db_connection = self::getDbConnection();
+        $statement = $db_connection->prepare($query);
+
+        if ($statement->execute($args)) {
+            return $statement->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        // close db connection
+        $statement = null;
+        $db_connection = null;
+
+        return false;
+    }
+
+    static public function selectOne($query, $args)
+    {
+        $db_connection = self::getDbConnection();
+        $statement = $db_connection->prepare($query);
+
+        if ($statement->execute($args)) {
+            return $statement->fetch(PDO::FETCH_ASSOC);
+        }
+
+        // close db connection
+        $statement = null;
+        $db_connection = null;
+
+        return false;
+    }
+
+    static public function insert($query, $args)
+    {
+        $db_connection = self::getDbConnection();
+        $statement = $db_connection->prepare($query);
+
+        if ($statement->execute($args)) {
+            return $db_connection->lastInsertId();
+        }
+
+        // close db connection
+        $statement = null;
+        $db_connection = null;
+
+        return false;
+
+    }
+
+    static public function execute($query, $args)
+    {
+        $db_connection = self::getDbConnection();
+        $statement = $db_connection->prepare($query);
+
+        if ($statement->execute($args)) {
+            return $statement->rowCount();
+        }
+
+        // close db connection
+        $statement = null;
+        $db_connection = null;
+
+        return false;
+    }
+}
