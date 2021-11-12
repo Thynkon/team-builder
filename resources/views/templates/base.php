40ad22ad750de5f60a0060ce6b78614319a4d155
thynkon <thynkon@protonmail.com>
2021-11-12 10:01:33 +0100

    Beginning of examn

--- resources/views/templates/base.php
+++ resources/views/templates/base.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $data["head"]["title"] ?></title>
+    <title>Version: Début Examen - Mário Ferreira</title>
     <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/pure-min.css"
           integrity="sha384-Uu6IeWbM+gzNVXJcM9XV3SohHtmWE+3VGi496jvgX1jyvDTXfdK+rfZc8C1Aehk5" crossorigin="anonymous">
     <link rel="stylesheet" href="https://unpkg.com/purecss@2.0.6/build/grids-responsive-min.css">
